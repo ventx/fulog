9f8afcfbd8b6525834898d1446d2ed584ad27c96
Wolfgang Felbermeier <felbermeier@bibchip.de>
2018-03-10 22:17:16 +0100

    start implementing directory structure tests

--- test/vfs.events.test.js
+++ test/vfs.events.test.js
@@ -1,6 +1,14 @@
 const expect = require('chai').expect;
 const eventWrapper = require('../src/vfs/events/');
+const DirectoryStructure = require('../src/vfs/projectors/DirectoryStructure');
 
+
+const AbstractProjectorMock = class {
+
+	constructor(){
+	}
+
+};
 
 describe("Virtual File System", function () {
 
@@ -8,6 +16,17 @@ describe("Virtual File System", function () {
 
 		it("dynamic resolution of events works", function () {
 			let directoryCreated = eventWrapper('DirectoryCreated', 'blubb', 'blargh');
+		});
+
+	});
+
+	describe("Directory Structure", function () {
+
+		it("can create a directory", function(){
+			Object.setPrototypeOf(DirectoryStructure, AbstractProjectorMock);
+			let directoryStructure = new DirectoryStructure();
+			directoryStructure.directoryCreated({path: '/bananarama/flurb', mode: 509});
+			expect(directoryStructure.getPath('/bananarama')).to.eql(['flurb']);
 		});
 
 	});
